9eb946880a68a267d2b7c385ed2d678877498051
bmagic007 <blake@mobiusconsortium.org>
2018-08-15 10:24:35 -0500

    Get AWS instance IP's and inject them into cssh config

---
+++ Random/getaws_retrieve_active_ips.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BEANSTALKTAG="nccard-AppEb-env"
+CSSHNAME="nccard"
+
+IPS=`aws ec2 describe-instances --filters "Name=tag:elasticbeanstalk:environment-name,Values=$BEANSTALKTAG"|grep '"PublicIp":'|awk '{print $2}'|sed -e 's/"//' -e 's/",//'|uniq|sed -e ':a;N;$!ba;s/\n/ /g'`
+
+START="$CSSHNAME "
+LINE=$START$IPS
+
+echo $LINE
+sed -i '/$CSSHNAME/d' .clusterssh/clusters
+
+echo $LINE >> .clusterssh/clusters
