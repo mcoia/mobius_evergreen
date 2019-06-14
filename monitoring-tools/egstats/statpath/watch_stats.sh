e906d543dfb0397ac18145e88bde3db59f68e416
Ted Peterson <ted@mobiusconsortium.org>
2019-06-14 12:11:06 -0500

    add instructions to restore screen

--- monitoring-tools/egstats/statpath/watch_stats.sh
+++
@@ -1,5 +0,0 @@
-# TO SETUP SCREEN:  /mnt/evergreen/statpath/screen_setup.sh
-# OR just run /mnt/evergreen/egstats, which runs "source /mnt/evergreen/statpath/screen_setup.sh"
-
-# restore screen
-screen -r node
